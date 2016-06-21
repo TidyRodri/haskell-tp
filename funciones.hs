584d81067e1fc226352134766f2d03ec8a842495
Agustín Argüello <agus.ap96@gmail.com>
2016-06-21 13:34:04 -0300

    Ejercicio 1/8

---
+++ funciones.hs
@@ -0,0 +1,15 @@
+import Data
+
+aplicarModificacion1 :: String -> Modificacion -> String
+aplicarModificacion1 str (Insertar index char) 
+    | index > (len str) || index < 0 = error "Out of range"
+    | index == (len str) = str ++ [char]
+    | otherwise = aplicarModificacion1 (init str) (Insertar index char) ++ [last str]
+aplicarModificacion1 str (Borrar index) 
+    | index > (len str) || index < 1 = error "Out of range"
+    | index == (len str) = init str
+    | otherwise = aplicarModificacion1 (init str) (Borrar index) ++ [last str]
+aplicarModificacion1 str (Substituir index char) 
+    | index > (len str) || index < 0 = error "Out of range"
+    | index == (len str) = init str ++ [char]
+    | otherwise = aplicarModificacion1 (init str) (Substituir index char) ++ [last str]

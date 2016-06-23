e9ee4fa2a9b227a8967097662ff661818a830d02
TidyRodri <rodrigo.apas99@gmail.com>
2016-06-22 22:42:30 -0300

    Agrego cantVersiones y levenshtein

--- funciones.hs
+++ funciones.hs
@@ -1,21 +1,34 @@
 import Data
 
-aplicarModificacion1 :: String -> Modificacion -> String
+aplicarModificacion :: String -> Modificacion -> String
 aplicarModificacion1 str (Insertar index char) 
     | index > (len str) || index < 0 = error "Out of range"
     | index == (len str) = str ++ [char]
-    | otherwise = aplicarModificacion1 (init str) (Insertar index char) ++ [last str]
-aplicarModificacion1 str (Borrar index) 
+    | otherwise = aplicarModificacion (init str) (Insertar index char) ++ [last str]
+aplicarModificacion str (Borrar index) 
     | index > (len str) || index < 1 = error "Out of range"
     | index == (len str) = init str
-    | otherwise = aplicarModificacion1 (init str) (Borrar index) ++ [last str]
-aplicarModificacion1 str (Substituir index char) 
+    | otherwise = aplicarModificacion (init str) (Borrar index) ++ [last str]
+aplicarModificacion str (Substituir index char) 
     | index > (len str) || index < 0 = error "Out of range"
     | index == (len str) = init str ++ [char]
-    | otherwise = aplicarModificacion1 (init str) (Substituir index char) ++ [last str]
+    | otherwise = aplicarModificacion (init str) (Substituir index char) ++ [last str]
 
 
 aplicarPaqueteModificaciones :: String -> PaqueteModificaciones -> String
 aplicarPaqueteModificaciones str modif  | (length modif) == 0 = str
                                         | (length modif) == 1 = (aplicarModificacion1 str (head modif))
                                         | otherwise = (aplicarPaqueteModificaciones (aplicarModificacion1 str (head modif)) (tail modif))
+                                    
+--contamos cada paquete de modificaciones
+cantVersiones :: Archivo -> Integer
+cantVersiones ArchivoVacio = 0 --no consideramos el empty file como version
+cantVersiones (NuevaVersion _ file) = 1 + cantVersiones file
+
+--calcado del algo del pddf
+levenshtein :: String -> String -> Integer
+levenshtein str1 str2 | (min (len str1) (len str2)) == 0 = max (len str1) (len str2)
+                      | (last str1) == (last str2)       = minimum [lev1 + 1,lev1 + 1, levenshtein (init str1) (init str2)]
+                      | otherwise                        = minimum [lev1 + 1,lev1 + 1, levenshtein (init str1) (init str2) + 1]
+                      where lev1 = levenshtein (init str1) str2
+                            lev2 = levenshtein str1 (init str2)
